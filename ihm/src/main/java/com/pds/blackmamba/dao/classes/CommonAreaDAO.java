3b68ea0d726569fd8fcaf3f837e6acbd6aee14f2
pierre-arnaudAlmeida <37550509+pierre-arnaudAlmeida@users.noreply.github.com>
2019-02-22 20:59:01 +0100

    v0.0.21
    Part2 pattern DAO

---
+++ ihm/src/main/java/com/pds/blackmamba/dao/classes/CommonAreaDAO.java
@@ -0,0 +1,36 @@
+package com.pds.blackmamba.dao.classes;
+
+import com.pds.blackmamba.dao.CommonArea;
+
+public class CommonAreaDAO extends DAO<CommonArea>{
+
+	public CommonAreaDAO() {
+		super();
+	}
+
+	@Override
+	public boolean create(CommonArea obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(CommonArea obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(CommonArea obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public CommonArea find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
