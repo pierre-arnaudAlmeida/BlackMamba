9771d9e9d07092800134443eb3774a19df2cd22f
pierre-arnaudAlmeida <37550509+pierre-arnaudAlmeida@users.noreply.github.com>
2019-03-11 16:48:57 +0100

    v0.0.27
    DAO super class + DAOFactory + JSONDAOFactory

---
+++ ihm/src/main/java/com/pds/blackmamba/dao/AbstractDAOFactory.java
@@ -0,0 +1,22 @@
+package com.pds.blackmamba.dao;
+
+public abstract class AbstractDAOFactory {
+	
+	public abstract DAO getBadgerDAO();
+	public abstract DAO getCommonAreaDAO();
+	public abstract DAO getEmployeeDAO();
+	public abstract DAO getManageDAO();
+	public abstract DAO getMonitorDAO();
+	public abstract DAO getReportDAO();
+	public abstract DAO getResidentDAO();
+	public abstract DAO getSensorDAO();
+	public abstract DAO getSensorHistoricalDAO();
+	
+	public static AbstractDAOFactory getFactory(FactoryType type) {
+		if(type.equals(type.DAO_FACTORY))
+			return new  DAOFactory();
+		if(type.equals(type.JSON_DAO_FACTORY))
+			return new JSONDAOFactory();
+		return null;
+	}
+}
